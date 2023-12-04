2914b2c0009714246a3fa625a588eac1dd5722ff
B-atuti <brian.atuti@student.moringaschool.com>
2023-12-04 15:27:50 +0300

    done

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 0);
 });
+document.getElementById('downloadCvBtn').addEventListener('click', function() {
+    window.open('media/brian.docx', '_blank');
+});
 
